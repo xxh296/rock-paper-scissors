90c5322edbfab8a176a5fee9dd4b0df316a42094
xxh296 <140650758+xxh296@users.noreply.github.com>
2024-12-19 10:26:50 -0800

    Add `getComputerChoice()`
    `getComputerChoice()` returns a result (`rock`, `paper`, or `scissors`)

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,2 +1,20 @@
-// DEBUG
-console.log("Test message");
+function getComputerChoice() {
+    // improve /3 precision
+    randomNumber = Math.random();
+    if (randomNumber === 0 || randomNumber === 1) {
+        getComputerChoice();
+    }
+    // DEBUG
+    console.log("randomNumber = " + randomNumber);
+
+    if (randomNumber <= 0.3333333333333333) {
+        return "rock";
+    } else if (randomNumber <= 0.6666666666666666) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+//DEBUG
+console.log(getComputerChoice());
